84e6256e02de349f970862e17d486113f1e05f51
ArsenSilver <vlad.sevast@mail.ru>
2021-09-15 21:03:14 +0300

    fixed poind distribution bug

--- task1/mainwindow.cpp
+++ task1/mainwindow.cpp
@@ -18,6 +18,7 @@ MainWindow::~MainWindow()
 
 void MainWindow::on_pushButton_clicked()
 {
+    close(); /*close GUI*/
     double param_a, param_b;
     param_a = ui->aParamEdit->toPlainText().toDouble();
     param_b = ui->bParamEdit->toPlainText().toDouble();
